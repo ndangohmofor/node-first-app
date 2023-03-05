a2aa398420b88fab212a189f3cb519779ecf2dc9
ndangohmofor <ndangohmofor@gmail.com>
2023-03-05 17:32:24 -0500

    Started on the express server

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
+// const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express(); //Request handler
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allow the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  //send response
+  res.send("<h1>Hellow from Express!</h1>");
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
