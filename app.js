61b1a0bacef051a721ee54882e7e47f9a92fef2b
ndangohmofor <ndangohmofor@gmail.com>
2023-03-05 19:58:56 -0500

    added two endpoints

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-// const http = require("http");
-// const routes = require("./routes");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express(); //Request handler
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); //Allow the request to continue to the next middleware in line
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
   //send response
-  res.send("<h1>Hellow from Express!</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' placeholder='Product' /><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  //send response
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 // const server = http.createServer(app);
