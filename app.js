d18d63b0e0fc3ed9810c3bcd44bd6e588ac2ed2c
ndangohmofor <ndangohmofor@gmail.com>
2023-03-05 21:18:11 -0500

    added code to return html files rather than html code. Added styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("./util/path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -8,11 +10,16 @@ const bodyParser = require("body-parser");
 const app = express(); //Request handler
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // const server = http.createServer(app);
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 // server.listen(3000);
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware");
   //send response
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' placeholder='Product' /><button type='submit'>Add Product</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
