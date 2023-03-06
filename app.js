bdfe8a766fd02db710828316eecc7568097c6f5e
ndangohmofor <ndangohmofor@gmail.com>
2023-03-05 20:08:51 -0500

    Created routes using the Router module from express

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const bodyParser = require("body-parser");
 
 const app = express(); //Request handler
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  //send response
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' placeholder='Product' /><button type='submit'>Add Product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  //send response
-  res.send("<h1>Hello from Express!</h1>");
-});
-
 // const server = http.createServer(app);
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 // server.listen(3000);
 app.listen(3000);
