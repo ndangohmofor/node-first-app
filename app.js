c7ae0aba7050d0bd4a38ab03cf5fd286a11efff7
ndangohmofor <ndangohmofor@gmail.com>
2023-03-12 08:16:51 -0400

    Created controllers for adding product and returning 404 error and migrated the logic from app.js to controller files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rootDir = require("./util/path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const bodyParser = require("body-parser");
 // const hbs = require("express-handlebars");
@@ -26,9 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 // server.listen(3000);
 app.listen(3000);
---
+++ models/product.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+    });
+  }
+
+  static fetchAll() {
+    return products;
+  }
+};
