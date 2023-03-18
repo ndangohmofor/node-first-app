14b2cd4f8968878993ca94ab5c8b22506613d030
ndangohmofor <ndangohmofor@gmail.com>
2023-03-18 11:19:05 -0400

    Added logic to delete a product from the shop, and also have the product removed from the cart and the total cart price reduced by the number of products deleted

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Cart = require('./cart')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -43,6 +44,18 @@ module.exports = class Product {
       });
       }
     });
+  }
+
+  static deleteById(id){
+    getProductsFromFile(products => {
+      const deletedProduct = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, deletedProduct.price);
+        }
+      })
+    })
   }
 
   static fetchAll(cb) {
