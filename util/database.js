0c743fa8b9ad545c79f8f7ad0a32e851ae8960e4
ndangohmofor <ndangohmofor@gmail.com>
2023-03-26 06:31:35 -0400

    Added sequelize as the ORM for mysql

--- util/database.js
+++ util/database.js
@@ -1,11 +1,7 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "shopadmin", "Karim_82", {
+  dialect: "mysql",
   host: "192.168.1.95",
-  port: "3306",
-  user: "shopadmin",
-  database: "node-complete",
-  password: "Karim_82",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
