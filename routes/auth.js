021038b7057b923771d8c685a3134e28dac58915
ndangohmofor <ndangohmofor@gmail.com>
2023-04-16 13:04:06 -0400

    More validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 const router = express.Router();
 
 const authController = require("../controllers/auth");
@@ -10,15 +10,23 @@ router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Enter valid Email")
-    .custom((value, { req }) => {
-      if (value === "test@test.com") {
-        throw new Error("This email address is forbidden");
-      }
-      return true;
-    }),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Enter valid Email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address is forbidden");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and characters and at least 5 characters long"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 router.get("/reset", authController.getReset);
